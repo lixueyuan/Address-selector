394062d3d71968e765d008f33d924549c57a7b3a
sTagRD <sTagRD@sTagRDdeMacBook-Air.local>
2018-01-18 13:44:15 +0800

    更新描述文件

--- 地址选择器/ViewController.m
+++ 地址选择器/ViewController.m
@@ -22,9 +22,9 @@
 
 - (IBAction)selectAddressAction:(id)sender {
     SelectCityViewController *selectVc = [[SelectCityViewController alloc] init];
-    
+    //设置模态弹出时的状态(避免黑屏)
     selectVc.modalPresentationStyle = UIModalPresentationOverCurrentContext;
-    
+    //选择完毕后点击确认按钮的回调
     selectVc.yourAddressBlock = ^(NSString *addressString) {
         [_addressButton setTitle:addressString forState:UIControlStateNormal];
     };
@@ -32,7 +32,6 @@
     if (![self.addressButton.titleLabel.text isEqualToString:@"老铁,选择地址"]) {
         selectVc.oldAddressString = self.addressButton.titleLabel.text;
     };
-    
     [self presentViewController:selectVc animated:YES completion:nil];
 }
 
